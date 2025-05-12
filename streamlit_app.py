c4218c0b0502bc953e5ca8e8a5e5988cc49d11fb
Joseph-722 <joseph.bautistaa@utem.cl>
2025-05-12 19:38:17 -0400

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 12 19:26:53 2025
+
+@author: Joseph
+"""
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+sns.set_style("whitegrid")
